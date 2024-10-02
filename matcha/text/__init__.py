362ba2dce73244443a97deb66587e1e8cf53f567
Jim O'Regan <joregan@kth.se>
2024-10-02 08:38:28 +0000

    C0209

--- matcha/text/__init__.py
+++ matcha/text/__init__.py
@@ -48,6 +48,6 @@ def _clean_text(text, cleaner_names):
     for name in cleaner_names:
         cleaner = getattr(cleaners, name)
         if not cleaner:
-            raise Exception("Unknown cleaner: %s" % name)
+            raise Exception(f"Unknown cleaner: {name}")
         text = cleaner(text)
     return text
