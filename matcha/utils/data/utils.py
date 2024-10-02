66bfbc106657eb66aa4fb32e537b1aee464d837b
Jim O'Regan <joregan@kth.se>
2024-10-02 12:29:53 +0000

    also accept Path

--- matcha/utils/data/utils.py
+++ matcha/utils/data/utils.py
@@ -5,14 +5,19 @@ import logging
 import os
 import tarfile
 import zipfile
-from typing import Any, List, Optional
+from pathlib import Path
+from typing import Any, List, Optional, Union
 
 _LG = logging.getLogger(__name__)
 
 
-def _extract_tar(from_path: str, to_path: Optional[str] = None, overwrite: bool = False) -> List[str]:
+def _extract_tar(from_path: Union[str, Path], to_path: Optional[str] = None, overwrite: bool = False) -> List[str]:
+    if type(from_path) == Path:
+        from_path = str(Path)
+
     if to_path is None:
         to_path = os.path.dirname(from_path)
+
     with tarfile.open(from_path, "r") as tar:
         files = []
         for file_ in tar:  # type: Any
@@ -27,7 +32,10 @@ def _extract_tar(from_path: str, to_path: Optional[str] = None, overwrite: bool 
         return files
 
 
-def _extract_zip(from_path: str, to_path: Optional[str] = None, overwrite: bool = False) -> List[str]:
+def _extract_zip(from_path: Union[str, Path], to_path: Optional[str] = None, overwrite: bool = False) -> List[str]:
+    if type(from_path) == Path:
+        from_path = str(Path)
+
     if to_path is None:
         to_path = os.path.dirname(from_path)
 
