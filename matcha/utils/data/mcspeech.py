cc791c6af42d7d6c4b62d4c8e9813f4c947a03a3
Jim O'Regan <joregan@kth.se>
2024-10-02 10:32:08 +0000

    unescaped backslash

--- matcha/utils/data/mcspeech.py
+++ matcha/utils/data/mcspeech.py
@@ -17,18 +17,18 @@ INFO_PAGE = "https://www.openslr.org/142/"
 
 LICENCE = "CC0 1.0"
 
-# CITATION = """
-# @masterthesis{mcspeech,
-#   title={Analiza porównawcza korpusów nagrań mowy dla celów syntezy mowy w języku polskim},
-#   author={Czyżnikiewicz, Mateusz},
-#   year={2022},
-#   month={December},
-#   school={Warsaw University of Technology},
-#   type={Master's thesis},
-#   doi={10.13140/RG.2.2.26293.24800},
-#   note={Available at \url{http://dx.doi.org/10.13140/RG.2.2.26293.24800}},
-# }
-# """
+CITATION = """
+@masterthesis{mcspeech,
+  title={Analiza porównawcza korpusów nagrań mowy dla celów syntezy mowy w języku polskim},
+  author={Czyżnikiewicz, Mateusz},
+  year={2022},
+  month={December},
+  school={Warsaw University of Technology},
+  type={Master's thesis},
+  doi={10.13140/RG.2.2.26293.24800},
+  note={Available at \\url{http://dx.doi.org/10.13140/RG.2.2.26293.24800}},
+}
+"""
 
 
 def decision():
